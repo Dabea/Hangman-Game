338a5329f11180806ac1d6bf6589c71d5f7e367c
Abraham <abeis@msn.com>
2018-02-28 22:20:12 -0600

    added buildblackSpaces method

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -3,8 +3,10 @@
 
 
 
+/**
+ * picks a random word form the list
 
-function pickRandomword (){
+function pickRandomword () {
     const wordBank = ['program', 'hello' , 'world'];
     const index = Math.floor(Math.random() * wordBank.length)  ;
     const chosenword = wordBank[index];
@@ -12,11 +14,56 @@ function pickRandomword (){
 }
 
 const chosenwod = pickRandomword();
-alert(chosenwod)
-
 const chosenWordLength = chosenwod.length;
 
 
 
 
-alert (chosenWordLength);
+*/
+
+
+/**
+ * HangMan Game constructor
+ * @constructor
+ */
+function Hangman(){
+    this.wordBank = ['program', 'hello' , 'world'];
+    this.bankSpaces = [];
+
+}
+
+/**
+ * This will chose a ranom word from the word bank
+ * 
+ * @for Hangman
+ * @method pickRandomWord
+ * @return string
+ */
+Hangman.prototype.pickRandomWord = function () {
+    const index = Math.floor(Math.random() * this.wordBank.length);
+    const chosenword = this.wordBank[index];
+    return chosenword;
+};
+
+/**
+ * This will chose a ranom word from the word bank
+ * 
+ * @for Hangman
+ * @method buildBlankSpaces
+ * @return array
+ */
+Hangman.prototype.buildBlankSpaces = function (word) {
+    for( let indexCounter = 0; indexCounter < word.length; indexCounter++){
+        this.bankSpaces.push('_');
+    }
+
+    alert(this.bankSpaces);
+
+};
+
+
+
+const game = new Hangman();
+
+game.pickRandomWord();
+game.buildBlankSpaces('hello');
