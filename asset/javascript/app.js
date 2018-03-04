a002d41691125134bff1fe7de9d159a9a8b5507b
Abraham <abeis@msn.com>
2018-03-03 19:33:39 -0600

    got base hangman functions working

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -8,7 +8,7 @@ function Hangman(){
     this.wordBank = ['program', 'hello' , 'world'];
     this.bankSpaces = [];
     this.activeWord = null;
-    this.activeWordLetters = null;
+    this.activeWordLetters = [];
     this.incorectGuess = null;
     this.incorectLetterBank = [];
 
@@ -69,6 +69,7 @@ Hangman.prototype.buildBlankSpaces = function (word) {
  */
 Hangman.prototype.incorrectLetterGuess = function (letter) {
     this.incorectGuess ++;
+    $('.incorect-guess').html(this.incorectGuess);
     this.incorectLetterBank.push(letter);
     //Draw Hangman
 
@@ -85,11 +86,18 @@ Hangman.prototype.incorrectLetterGuess = function (letter) {
  * @method corectLetterGuessed
  *
  */
-Hangman.prototype.corectLetterGuessed = function (word) {
-   
-};
+Hangman.prototype.corectLetterGuessed = function (letter) {
+   const indexOfLetters = this.allIndexOf(this.activeWord, letter);
+   for(let i = 0 ; i < indexOfLetters.length; i++ ){
+    this.bankSpaces[indexOfLetters[i]] = letter;
+    console.log(this.bankSpaces);
+   }
 
-Hangman.prototype.gameOVer = function (){
+   this.drawWord();
+
+}
+
+Hangman.prototype.gameOver = function (){
     //ends the game
 }
 
@@ -99,27 +107,49 @@ Hangman.prototype._setActiveLetters = function(activeWord) {
 }
 
 Hangman.prototype.drawWord = function ($element) {
+    $element = $('.word-area');
+    $element.html('');
     for(let indexCounter = 0; indexCounter < this.activeWord.length ; indexCounter++){
+        console.log('draw');
         $(`<div class='letter-display'>${this.bankSpaces[indexCounter]}</div>`).appendTo($element).hide().fadeIn(300 * indexCounter);
     }
 }
 
 Hangman.prototype.enable = function() {
-    this.guess = this.guess.bind(this);
+    let gussesEvent = this.guess.bind(this);
 
     $('.keyboard').on('click', 'div.key', function(){
-        alert('a Key is pressed');
-    })
+        gussesEvent(event);
+    });
 
-    $('.test').on('click' , {letter: 'o'}, this.guess)
+    $('.test').on('click' , {letter: 'o'}, this.guess);
+    $(document).on('keypress' ,  gussesEvent);
 };
 
 Hangman.prototype.guess = function(event) {
-    alert(event.data.letter);
+    const letter = event.target.dataset.letter || event.key;
+    if(this.activeWordLetters.includes(letter)){
+       this.corectLetterGuessed(letter);
+    }else{
+        this.incorrectLetterGuess(letter);
+    }
 };
 
+
+Hangman.prototype.allIndexOf = function (string, toSearch) {
+    var indices = [];
+    for(var pos = string.indexOf(toSearch); pos !== -1; pos = string.indexOf(toSearch, pos + 1)) {
+        indices.push(pos);
+    }
+    return indices;
+}
+
 const game = new Hangman();
+
+
 
 $( document ).ready(function() {
     game.init();
 });
+
+
